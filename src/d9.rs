c261445fc5b3bf98a93dd5bc5411143199cede31
Spencer Rak <spencer.rak@hadronindustries.com>
2025-12-28 08:45:31 -0500

    Snapshot

--- src/d9.rs
+++ src/d9.rs
@@ -3,6 +3,7 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::ops::Sub;
 use std::fmt;
+use std::boxed::Box;
 
 type Point = (i64, i64);
 
@@ -46,9 +47,9 @@ impl Polygon {
                 //println!("Point {:?} sits on an edge, short-circuit true", p);
                 return true;
             }
-            println!("Evaluate {:?}, {:?}, {:?}, {:?}", p, p2, b1, b2);
-            println!("Evaluate {} <= {}, {} >= {}, {} < {}, {} > {}", p.0, b1.0, p2.0, b1.0, p.1, b2.1, p.1, b1.1);
-            println!("Evaluate {}, {}, {}, {}", (p.0 <= b1.0), (p2.0 >= b1.0), (p.1 < b2.1), (p.1 > b1.1));
+            //println!("Evaluate {:?}, {:?}, {:?}, {:?}", p, p2, b1, b2);
+            //println!("Evaluate {} <= {}, {} >= {}, {} < {}, {} > {}", p.0, b1.0, p2.0, b1.0, p.1, b2.1, p.1, b1.1);
+            //println!("Evaluate {}, {}, {}, {}", (p.0 <= b1.0), (p2.0 >= b1.0), (p.1 < b2.1), (p.1 > b1.1));
             if p.1 == b2.1 || p.1 == b1.1 {
                 shouldve += 1;
                 println!("Should've {:?}, {:?}, {:?}", &p, &b1, &b2);
@@ -59,28 +60,74 @@ impl Polygon {
                 && p.1 < b2.1
                 && p.1 > b1.1
             ) {
-                println!("Found line intersection!");
+                println!("Found intersect {:?}, {:?}, {:?}", &p, &b1, &b2);
                 intersects += 1;
             }
         }
-        println!("Returning intersection {}", (intersects % 2 == 1 || shouldve % 2 == 1));
-        return intersects % 2 == 1 || shouldve % 2 == 1;
+        let ret = (intersects % 2 == 1 || shouldve % 2 == 1);
+        println!("Returning intersection {}", ret);
+        return ret;
     }
 
-    fn contains(&mut self, p: Point) -> bool {
+    fn contains(&mut self, p: &Point) -> bool {
         // we cache our finds to hopefully gain later
-        match self.cache.get(&p) {
+        match self.cache.get(p) {
             Some(x) => {
+                // got ~250k cache-hits so was worth
                 //println!("Cache-HIT {:?}", &p);
                 return x.clone();
             }
             None => {
-                println!("Checking point {:?}", &p);
-                let b = self.contained(&p);
-                self.cache.insert(p, b);
+                println!("Checking point {:?}", p);
+                let b = self.contained(p);
+                self.cache.insert((*p).clone(), b);
                 return b;
             }
         };
+    }
+
+    fn points_for(a: &Point, b: &Point) -> Box<dyn Iterator<Item=Point>> {
+        if a.0 < b.0 {
+            // then c is also left
+            if a.1 < b.1 {
+                let c = (b.0, a.1);
+                let d = (a.0, b.1);
+                // then a, c, b, d
+                //println!("A is close to origin, Order a,c,b,d");
+                //println!("Returning points {:?}, {:?}, {:?}, {:?}",
+                //    (a.0+1, a.1), (c.0, c.1+1), (b.0-1, b.1), (d.0, d.1-1));
+                return Box::new([(a.0+1, a.1), (c.0, c.1+1), (b.0-1, b.1), (d.0, d.1-1)].into_iter())
+                
+            } else {
+                let c = (a.0, b.1);
+                let d = (b.0, a.1);
+                // then c, b, d, a
+                //println!("C is close to origin, Order c,b,d,a");
+                //println!("Returning points {:?}, {:?}, {:?}, {:?}",
+                //    (c.0+1, c.1), (b.0, b.1+1), (d.0-1, d.1), (a.0, a.1-1));
+                return Box::new([(c.0+1, c.1), (b.0, b.1+1), (d.0-1, d.1), (a.0, a.1-1)].into_iter())
+            }
+        } else {
+            // then c is also right
+            if a.1 < b.1 {
+                let c = (a.0, b.1);
+                let d = (b.0, a.1);
+                // d, a, c, b
+                //println!("D is close to origin, Order d,a,c,b");
+                //println!("Returning points {:?}, {:?}, {:?}, {:?}", d, a, c, b);
+                //println!("Modified points {:?}, {:?}, {:?}, {:?}",
+                //    (d.0+1, d.1), (a.0, a.1+1), (c.0-1, c.1), (b.0, b.1-1));
+                return Box::new([(d.0+1, d.1), (a.0, a.1+1), (c.0-1, c.1), (b.0, b.1-1)].into_iter())
+            } else {
+                let c = (b.0, a.1);
+                let d = (a.0, b.1);
+                // b, d, a, c
+                //println!("B is close to origin, Order b,d,a,c");
+                //println!("Returning points {:?}, {:?}, {:?}, {:?}",
+                //    (b.0+1, b.1), (d.0, d.1+1), (a.0-1, a.1), (c.0, c.1-1));
+                return Box::new([(b.0+1, b.1), (d.0, d.1+1), (a.0-1, a.1), (c.0, c.1-1)].into_iter())
+            }
+        }
     }
 
     fn area(&mut self, adx: usize, bdx: usize) -> Option<i64> {
@@ -107,33 +154,13 @@ impl Polygon {
             return Some((a.1 - b.1).abs() + 1);
         }
 
-        if a.0 > b.0 {
-            t = a.0;
-            a.0 = b.0;
-            b.0 = t;
-        }
-        if a.1 > b.1 {
-            t = a.1;
-            a.1 = b.1;
-            b.1 = t;
-        }
         //println!("Checking rect {:?}:{:?}", a, b);
 
-        //println!("Checking {:?}", (a.0+1, a.1));
-        if !self.contains((a.0+1, a.1)) {
-            return None;
-        }
-        //println!("Checking {:?}", (b.0, a.1+1));
-        if !self.contains((b.0, a.1+1)) {
-            return None;
-        }
-        //println!("Checking {:?}", (b.0-1, b.1));
-        if !self.contains((b.0-1, b.1)) {
-            return None;
-        }
-        //println!("Checking {:?}", (a.0, b.1-1));
-        if !self.contains((a.0, b.1-1)) {
-            return None;
+        for p in Self::points_for(&a, &b) {
+            //println!("Checking {:?}", p);
+            if !self.contains(&p) {
+                return None;
+            }
         }
         //println!("Rect area of {:?},{:?}", &a, &b);
         //println!("Rect area {} * {}", ((a.0 - b.0).abs() + 1), ((a.1 - b.1).abs() + 1));
@@ -192,26 +219,12 @@ pub fn run() {
 
     poly.cache_verticals();
 
-    // to the inside corner of the 
-    // maybe we check every point but cache them in a map as we go
-    // but how do we decide whether a point is inside or outside
-    // do we use the ray method
-    // worst case we check every point in the rectangle for being inside our
-    // shape
-    //
-    // we fail fast if we find a point outside our perimeter
-    //
-    // so we generate all of the "red-rectangles" and begin determining
-    // whether each point is inside or outside our shape
-    //
-
     let mut num_rect: i64 = 0;
     let mut valid_rect: i64 = 0;
     let mut big_a: i64 = 0;
 
     for i in 0..poly.points.len() {
         for j in i+1..poly.points.len() {
-            assert_ne!(i, j);
             num_rect += 1;
             match poly.area(i, j) {
                 Some(x) => {
@@ -219,6 +232,7 @@ pub fn run() {
                     if x > big_a {
                         //println!("Found big a!");
                         big_a = x;
+                        println!("BIG {} constructed from {:?},{:?}", big_a, &poly.points[i], &poly.points[j]);
                     }
                 }
                 None => (),//println!("Points {:?} and {:?} don't pass intersection", &poly.points[i], &poly.points[j]),
@@ -266,28 +280,28 @@ mod test {
     #[test]
     fn test_not_contains() {
         let mut p = setup_polygon();
-        assert_eq!(p.contains((12,6)), false);
-        assert_eq!(p.contains((1,6)), false);
+        assert_eq!(p.contains(&(12,6)), false);
+        assert_eq!(p.contains(&(1,6)), false);
     }
 
     #[test]
     fn test_contains() {
         let mut p = setup_polygon();
-        assert_eq!(p.contains((11,2)), true);
-        assert_eq!(p.contains((9,6)), true);
-        assert_eq!(p.contains((3,3)), true);
+        assert_eq!(p.contains(&(11,2)), true);
+        assert_eq!(p.contains(&(9,6)), true);
+        assert_eq!(p.contains(&(3,3)), true);
     }
 
     #[test]
     fn test_origin_contains() {
         let mut p = setup_polygon();
-        assert_eq!(p.contains((2,3)), true);
+        assert_eq!(p.contains(&(2,3)), true);
     }
 
     #[test]
     fn test_maximum_contains() {
         let mut p = setup_polygon();
-        assert_eq!(p.contains((11,7)), true);
+        assert_eq!(p.contains(&(11,7)), true);
     }
 
     #[test]
@@ -310,5 +324,48 @@ mod test {
         assert_eq!(p.area(4, 6), Some(24));
         assert_eq!(p.area(1, 2), Some(7));
         assert_eq!(p.area(1, 4), Some(15));
+    }
+
+    #[test]
+    fn test_points_for_2_x_2() {
+        let mut iter = Polygon::points_for(&(1,1), &(2,2));
+        assert_eq!(iter.next(), Some((2,1)));
+        assert_eq!(iter.next(), Some((2,2)));
+        assert_eq!(iter.next(), Some((1,2)));
+        assert_eq!(iter.next(), Some((1,1)));
+    }
+
+    #[test]
+    fn test_points_acbd() {
+        let mut iter = Polygon::points_for(&(1,1), &(3,3));
+        assert_eq!(iter.next(), Some((2,1)));
+        assert_eq!(iter.next(), Some((3,2)));
+        assert_eq!(iter.next(), Some((2,3)));
+        assert_eq!(iter.next(), Some((1,2)));
+    }
+
+    #[test]
+    fn test_points_cbda() {
+        let mut iter = Polygon::points_for(&(1,3), &(3,1));
+        assert_eq!(iter.next(), Some((2,1)));
+        assert_eq!(iter.next(), Some((3,2)));
+        assert_eq!(iter.next(), Some((2,3)));
+        assert_eq!(iter.next(), Some((1,2)));
+    }
+    #[test]
+    fn test_points_bdac() {
+        let mut iter = Polygon::points_for(&(3,3), &(1,1));
+        assert_eq!(iter.next(), Some((2,1)));
+        assert_eq!(iter.next(), Some((3,2)));
+        assert_eq!(iter.next(), Some((2,3)));
+        assert_eq!(iter.next(), Some((1,2)));
+    }
+    #[test]
+    fn test_points_dacb() {
+        let mut iter = Polygon::points_for(&(3,1), &(1,3));
+        assert_eq!(iter.next(), Some((2,1)));
+        assert_eq!(iter.next(), Some((3,2)));
+        assert_eq!(iter.next(), Some((2,3)));
+        assert_eq!(iter.next(), Some((1,2)));
     }
 }
