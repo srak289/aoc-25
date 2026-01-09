ef0ad81c228499b150e207638c716a620f03b3e3
Spencer Rak <spencer.rak@hadronindustries.com>
2026-01-09 12:54:53 -0500

    Snapshot

--- d10/src/p2.rs
+++ d10/src/p2.rs
@@ -1,13 +1,14 @@
 // allow all warnings for development
 #![allow(warnings)]
 
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::iter::zip;
 
 use regex::Regex;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
 struct SimpleVector {
     arr: [u16; 10],
 }
@@ -25,27 +26,19 @@ impl SimpleVector {
         SimpleVector { arr: a }
     }
 
-    fn sub(&self, other: &SimpleVector) -> SimpleVector {
-        let mut a = [0u16; 10];
-        for i in 0..10 {
-            a[i] = self.arr[i] - other.arr[i];
-        }
-        SimpleVector { arr: a }
+    fn eq(&self, other: &SimpleVector) -> bool {
+        return self.arr.iter().zip(other.arr.iter()).all(|(&a, &b)| a == b);
     }
 
-    fn euclidean_dist(&self, other: &SimpleVector) -> f64 {
+    fn distance_from_target(&self, target: &SimpleVector) -> f64 {
         let sum_of_squares: f64 = self.arr.iter()
-            .zip(other.arr.iter())
+            .zip(target.arr.iter())
             .map(|(&a, &b)| {
                 let diff = (a as f64) - (b as f64);
                 diff * diff
             })
             .sum();
-        sum_of_squares.sqrt()
-    }
-
-    fn distance_from_target(&self, origin: &SimpleVector, target: &SimpleVector) -> f64 {
-        return (origin.add(self)).euclidean_dist(target);
+        return sum_of_squares.sqrt();
     }
 }
 
@@ -77,68 +70,102 @@ impl Machine {
         }
     }
 
-    fn overjolt(&self, button: usize) -> bool {
-        for ((i, j), k) in zip(zip(self.target.arr, self.jolts.arr), self.buttons[button].arr) {
-            if i == 0 {
-                continue;
+    fn do_vector_dfs(&self) -> usize {
+        // best should just return the best button from our current position unless we've already
+        // explored that path
+        // we should reimplement hashset to cache explored paths
+        // we need a failure condition so that we know when to walk back
+        // we can probably use recursion using this vector best-path algorithm
+        //
+        // maybe we make the best path smart enough to realize which buttons have already been
+        // pushed from a given node and stop checking them so it still returns the best unchecked
+        // path when asked for the best path
+        //
+        // if a button returns overjolt we should mark that path fully explored
+
+        fn overjolt(target: &SimpleVector, node: &SimpleVector, button: &SimpleVector) -> bool {
+            // b is the button we will push
+            for ((i, j), k) in zip(zip(target.arr, node.arr), button.arr) {
+                if i == 0 {
+                    // target only has zero values after relevant dimensions
+                    continue;
+                }
+                // println!("Trying {} < ({} + {})", i, j, k);
+                if i < (j + k) {
+                    // println!("Node {:?} + {:?} is overjolt", node, button);
+                    return true;
+                }
             }
-            // println!("Trying {} < ({} + {})", i, j, k);
-            if i < (j + k) {
-                println!("Button {} is overjolt", button);
-                return true;
-            }
+            return false;
         }
-        return false;
-    }
 
-    fn best(&mut self) {
-        let mut bestidx: Option<usize> = None;
-        let mut curval: Option<f64> = None;
-        let mut bestval: Option<f64> = None;
+        fn best(target: &SimpleVector, children: &mut Vec<SimpleVector>) {
+            children.sort_by(|a, b| a.distance_from_target(&target).partial_cmp(&b.distance_from_target(&target)).unwrap());
+        }
 
-        // println!("Starting with button {:?} {:?}", &self.buttons[0], bestval);
-        for i in 0..self.buttons.len() {
-            if self.overjolt(i) {
-                // println!("{:?} is overjolt", self.buttons[i]);
-                continue;
-            }
-            // println!("Trying button {:?}", &self.buttons[i]);
-            curval = Some(self.buttons[i].distance_from_target(&self.jolts, &self.target));
-            // println!("Found curval {}", curval.unwrap());
-            match bestval {
-                Some(x) => {
-                    // println!("Try {} < {}", curval.unwrap(), x);
-                    if curval.unwrap() < x {
-                        bestidx = Some(i);
-                        bestval = curval;
-                        // println!("Found better button {} < {}", curval.unwrap(), x);
+        fn descend(done: &mut bool, node: &SimpleVector, adjlist: &mut HashMap<SimpleVector, Vec<SimpleVector>>, depth: &mut usize, buttons: &Vec<SimpleVector>, target: &SimpleVector) {
+            // println!("Depth is {}", depth);
+            *depth += 1;
+
+            // println!("{:?}", adjlist);
+            if adjlist.get(node) == None {
+                // node unprovisioned
+                // println!("Unprovisioned node {:?}", node);
+                let mut vnv = Vec::<SimpleVector>::with_capacity(buttons.len());
+                //
+                // make the child nodes
+                for button in buttons {
+                    // don't explore overjolt nodes
+                    if overjolt(&target, &node, &button) {
+                        continue;
                     }
+                    vnv.push(node.add(&button));
                 }
-                None => {
-                    bestval = curval;
-                    bestidx = Some(i);
+                // println!("Added nodes {:?}", vnv);
+
+                if vnv.len() == 0 {
+                    println!("Discovered leaf {:?} at depth {}", node, depth);
+                    return;
                 }
+                // let d = vnv.clone().into_iter().map(|x: SimpleVector| x.distance_from_target(&target)).collect::<Vec<f64>>();
+                // println!("VNV {:?}", d);
+                best(&target, &mut vnv);
+                // let d = vnv.clone().into_iter().map(|x: SimpleVector| x.distance_from_target(&target)).collect::<Vec<f64>>();
+                // println!("VNV {:?}", d);
+                // println!("Inserting {:?} with {:?}", node, vnv);
+                let _ = adjlist.insert(node.clone(), vnv.clone());
             }
+
+            let r = adjlist.get(node).unwrap();
+            // check the adjacencies
+            for vn in r.clone() {
+                if *done {
+                    break;
+                }
+                if vn.eq(target) {
+                    println!("Found target!! {:?} == {:?} at depth {}", vn, target, depth);
+                    *done = true;
+                    return;
+                }
+                // let vn = &best(&target, &x);
+                // println!("Recursing into {:?}", vn);
+                descend(done, &vn, adjlist, depth, &buttons, &target);
+            }
+            *depth -= 1;
         }
 
-        match bestval {
-            Some(x) => {
-                // println!("Jolts is {:?}", &self.jolts);
-                // println!("Adding button {:?} to jolts", &self.buttons[bestidx.unwrap()]);
-                self.jolts = self.jolts.add(&self.buttons[bestidx.unwrap()]);
-                // println!("Target is {:?}", &self.target);
-                println!("Pushed button {}", bestidx.unwrap());
-                println!("Jolts is now {:?}", &self.jolts);
-            }
-            None => {
-                println!("Did not find a better button");
-            }
-        }
+        let mut depth: usize = 0;
+        let mut adjlist = HashMap::<SimpleVector, Vec<SimpleVector>>::with_capacity(256);
+        let mut done = false;
+
+        descend(&mut done, &self.jolts.clone(), &mut adjlist, &mut depth, &self.buttons, &self.target);
+        println!("Target {:?} reached at depth {}", &self.target, depth);
+        return depth;
     }
 }
 
 fn main() {
-    let mut reader = BufReader::new(File::open("factory_sample.txt").expect("reading file failed"));
+    let mut reader = BufReader::new(File::open("factory.txt").expect("reading file failed"));
     let mut line = String::new();
 
     let mut machines = Vec::<Machine>::new();
@@ -158,11 +185,10 @@ fn main() {
                 let buttons: Vec::<&str> = bre.find_iter(&line).map(|m| m.as_str()).collect();
                 let jolts = jre.find(&line);
 
-                let mut l: u16 = 0;
+                let mut l = 0_u16;
                 let mut jv = [0u16; 10];
 
                 for (i, j) in jolts.unwrap().as_str().strip_prefix('{').unwrap().strip_suffix('}').unwrap().split(',').enumerate() {
-                    println!("{}", j);
                     jv[i] = j.parse::<u16>().unwrap();
                 }
                 // println!("Constructed jolt target {:?}", &jv);
@@ -188,12 +214,8 @@ fn main() {
         }
     }
 
-    for _ in 0..10 {
-        machines[0].best();
-    }
-    // for mut m in machines {
-    //     // println!("{:?}", m);
-    // }
+    machines[0].do_vector_dfs();
+    // println!("Ans {}", machines.into_iter().map(|m| m.do_vector_dfs()).sum::<usize>());
 }
 
 #[cfg(test)]
